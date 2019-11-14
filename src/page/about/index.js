212718cbbbc32a31ce1ecc09f5429370bb658f83
xin.duan <xin.duan@cygia.com>
2019-11-15 00:24:35 +0800

    about

---
+++ src/page/about/index.js
@@ -0,0 +1,9 @@
+/**
+ *
+ * @Date 2019-11-15 12:23
+ * @Author zone
+ */
+
+ 'use strict';
+require('page/common/header/index.js');
+require('page/common/nav/index.js');
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,7 @@ var getHtmlConfig = function(name, title){
     return {
         template    : './src/view/' + name + '.html',
         filename    : 'view/' + name + '.html',
+        favicon     : './favicon.ico',
         title       : title,
         inject      : true,
         hash        : true,
@@ -36,11 +37,13 @@ var config = {
         'user-center'           : ['./src/page/user-center/index.js'],
         'user-center-update'    : ['./src/page/user-center-update/index.js'],
         'user-pass-update'      : ['./src/page/user-pass-update/index.js'],
-        'result'                : ['./src/page/result/index.js']
+        'result'                : ['./src/page/result/index.js'],
+        'about'                 : ['./src/page/about/index.js']
     },
     output:{
-        path: __dirname+'/dist',
+        path: __dirname+'/dist/',
         publicPath : '/dist/',
+        // publicPath : 'dev' === WEBPACK_ENV ? '/dist/' : '//s.zonedx.cn/zmall-fe/dist/',
         filename: 'js/[name].js'
     },
     externals: {
@@ -60,7 +63,14 @@ var config = {
             // { test: /\.css$/, loader: "style-loader!css-loader" }
             { test: /\.css$/, loader: ExtractTextPlugin.extract({fallback:"style-loader",use:"css-loader"}) },
             { test: /\.(gif|png|jpg|woff|svg|eot|ttf)\??.*$/, loader: 'url-loader?limit=100&name=resource/[name].[ext]' },
-            { test: /\.string$/, loader: 'html-loader'}
+            { 
+                test: /\.string$/, 
+                loader: 'html-loader',
+                query : {
+                    minimize :true,
+                    removeAttributeQuotes : false
+                }
+            }
         ]
     },
     resolve : {
@@ -97,6 +107,7 @@ var config = {
         new HtmlWebpackPlugin(getHtmlConfig('user-center-update', '修改个人信息')),
         new HtmlWebpackPlugin(getHtmlConfig('user-pass-update', '修改密码')),
         new HtmlWebpackPlugin(getHtmlConfig('result', '操作结果')),
+        new HtmlWebpackPlugin(getHtmlConfig('about', '关于我们'))
     ]
 };
 
